1d8159b242fc773e4200cc493b757c57295c2dd6
STIVENSON.DEV <96761629+StivensonSS@users.noreply.github.com>
2022-06-27 23:16:21 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let altura = "193";
+console.log(parseInt(altura));
+
+let alturaFloat = "1.93 CM";
+console.log(parseFloat(alturaFloat));
+
+let peso = "108.6 kilos";
+console.log(parseFloat(peso));
+
+let alturaRedondeada = 193.51
+console.log(Math.round(alturaRedondeada));
+
+let pesoRedondeado = 108.6
+console.log(Math.floor(pesoRedondeado));
+
+let maxValor = 
